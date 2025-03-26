f13c3ad8375d274cecf270002f68918d417d0c92
Kajin-0 <terencebfisher@gmail.com>
2025-03-26 17:47:59 -0400

    Add files via upload

--- materials.js
+++ materials.js
@@ -1,205 +1,236 @@
 window.materials = [
   {
-    "name": "Gallium Arsenide",
-    "formula": "GaAs",
+    "name": "Reinforced Carbon\u2013Carbon (RCC)",
+    "formula": "C\u2013C + SiC",
     "synonyms": [
-      "GaAs",
-      "Gallium Arsenide"
-    ],
-    "category": "Semiconductor",
-    "tags": [
-      "III-V",
-      "group 13",
-      "group 15",
-      "photodiode",
-      "photosensor",
-      "photosensing",
-      "direct bandgap",
-      "semiconductor",
-      "high mobility",
-      "infrared",
-      "SWIR",
-      "epitaxial",
-      "MBE",
-      "MOCVD",
-      "single crystal",
-      "CMOS compatible",
-      "laser diode",
-      "sensor",
-      "commercial",
-      "aerospace",
-      "cryogenic",
-      "vacuum compatible"
-    ]
-  },
-  {
-    "name": "Silicon",
-    "formula": "Si",
-    "synonyms": [
-      "Si",
-      "Silicon"
-    ],
-    "category": "Semiconductor",
-    "tags": [
-      "Group IV",
-      "elemental",
-      "indirect bandgap",
-      "semiconductor",
-      "CMOS compatible",
-      "transistor",
-      "substrate",
-      "high temperature",
-      "durable",
-      "low cost",
-      "aerospace",
-      "computing",
-      "research",
-      "bulk grown"
-    ]
-  },
-  {
-    "name": "Mercury Cadmium Telluride",
-    "formula": "Hg1-xCdxTe",
-    "synonyms": [
-      "HgCdTe",
-      "MCT"
-    ],
-    "category": "Semiconductor",
-    "tags": [
-      "II-VI",
-      "group 12",
-      "group 16",
-      "chalcogenide",
-      "IR",
-      "MWIR",
-      "LWIR",
-      "photosensor",
-      "photodiode",
-      "photoconductor",
-      "photoconductive",
-      "tunable bandgap",
-      "semiconductor",
-      "MBE",
-      "LPE",
-      "epitaxial",
-      "single crystal",
-      "mercury",
-      "cadmium",
-      "tellurium",
-      "toxic",
-      "cryogenic",
-      "nitrogen cooled",
-      "aerospace",
-      "sensor"
-    ]
-  },
-  {
-    "name": "Aluminum Oxide",
-    "formula": "Al2O3",
-    "synonyms": [
-      "Sapphire",
-      "Alumina"
-    ],
-    "category": "Ceramic",
-    "tags": [
-      "oxide",
-      "insulator",
-      "hard",
-      "corrosion resistant",
-      "thermal insulator",
-      "transparent",
-      "substrate",
-      "aerospace",
-      "vacuum compatible",
-      "durable",
-      "ceramic"
-    ]
-  },
-  {
-    "name": "Indium Gallium Arsenide",
-    "formula": "InGaAs",
-    "synonyms": [
-      "InxGa1-xAs",
-      "InGaAs"
-    ],
-    "category": "Semiconductor",
-    "tags": [
-      "III-V",
-      "group 13",
-      "group 15",
-      "infrared",
-      "SWIR",
-      "photosensor",
-      "photodiode",
-      "semiconductor",
-      "MBE",
-      "MOCVD",
-      "epitaxial",
-      "tunable bandgap",
-      "high mobility",
-      "sensor"
-    ]
-  },
-  {
-    "name": "Titanium Alloy",
-    "formula": "Ti-6Al-4V",
-    "synonyms": [
-      "Ti64",
-      "Grade 5 Titanium"
-    ],
-    "category": "Metal",
-    "tags": [
-      "strong",
-      "ductile",
-      "corrosion resistant",
-      "aerospace",
-      "metal",
-      "vacuum compatible",
-      "high temperature",
-      "machinable",
-      "biocompatible",
-      "alloy"
-    ]
-  },
-  {
-    "name": "Carbon Fiber Reinforced Polymer",
-    "formula": "CFRP",
-    "synonyms": [
-      "Carbon Fiber Composite"
+      "RCC",
+      "Reinforced Carbon Composite"
     ],
     "category": "Composite",
     "tags": [
       "composite",
-      "lightweight",
-      "strong",
       "aerospace",
-      "structure",
-      "durable",
-      "high strength",
-      "low density",
-      "thermal stable",
-      "non-metallic"
+      "thermal protection",
+      "oxidation resistant",
+      "refractory",
+      "heat shield",
+      "space shuttle",
+      "carbon-carbon",
+      "high temperature",
+      "structural",
+      "industry:aerospace",
+      "industry:defense",
+      "industry:space"
     ]
   },
   {
-    "name": "Bismuth Telluride",
-    "formula": "Bi2Te3",
+    "name": "LI-900 Silica Tile",
+    "formula": "SiO\u2082 foam",
     "synonyms": [
-      "BiTe",
-      "Thermoelectric"
+      "LI-900",
+      "Silica Tile",
+      "Thermal Protection Tile"
     ],
-    "category": "Semiconductor",
+    "category": "Composite",
     "tags": [
-      "thermoelectric",
-      "semiconductor",
-      "cooling",
-      "generator",
-      "chalcogenide",
-      "group 15",
-      "group 16",
-      "vacuum compatible",
+      "aerospace",
+      "heat shield",
+      "lightweight",
+      "reusable",
+      "thermal insulation",
+      "low density",
+      "TPS",
+      "insulator",
+      "porous",
+      "high temperature",
+      "industry:aerospace",
+      "industry:space"
+    ]
+  },
+  {
+    "name": "Yttrium Barium Copper Oxide (YBCO)",
+    "formula": "YBa\u2082Cu\u2083O\u2087",
+    "synonyms": [
+      "YBCO",
+      "HTS tape",
+      "coated conductor"
+    ],
+    "category": "Superconductor",
+    "tags": [
+      "superconductor",
+      "HTS",
+      "quantum computing",
+      "coated conductor",
+      "low temperature",
+      "oxide",
+      "ceramic",
       "cryogenic",
-      "aerospace"
+      "power electronics",
+      "microwave",
+      "industry:quantum",
+      "industry:energy",
+      "industry:cryogenics"
+    ]
+  },
+  {
+    "name": "TUFROC",
+    "formula": "B/SiC/SiO\u2082 matrix",
+    "synonyms": [
+      "TUFROC",
+      "TPS composite"
+    ],
+    "category": "Composite",
+    "tags": [
+      "composite",
+      "TPS",
+      "thermal protection",
+      "aerospace",
+      "refractory",
+      "heat shield",
+      "toughened",
+      "oxidation resistant",
+      "space reentry",
+      "industry:aerospace",
+      "industry:space"
+    ]
+  },
+  {
+    "name": "Yttrium Iron Garnet (YIG)",
+    "formula": "Y\u2083Fe\u2085O\u2081\u2082",
+    "synonyms": [
+      "YIG",
+      "Magnetic Garnet"
+    ],
+    "category": "Composite",
+    "tags": [
+      "magnetic",
+      "quantum magnonics",
+      "microwave",
+      "low damping",
+      "spin wave",
+      "cryogenic",
+      "photonics",
+      "quantum computing",
+      "ferrite",
+      "industry:quantum",
+      "industry:photonics",
+      "industry:microwave"
+    ]
+  },
+  {
+    "name": "Nextel 720 Fiber",
+    "formula": "Alumina-Boria-Silica",
+    "synonyms": [
+      "Nextel",
+      "Ceramic Fiber 720"
+    ],
+    "category": "Composite",
+    "tags": [
+      "ceramic fiber",
+      "aerospace",
+      "thermal insulation",
+      "refractory",
+      "high temperature",
+      "structural",
+      "flexible",
+      "non-metallic",
+      "industry:aerospace",
+      "industry:infrastructure"
+    ]
+  },
+  {
+    "name": "Sapphire Substrate",
+    "formula": "Al\u2082O\u2083 (C-plane)",
+    "synonyms": [
+      "C-plane Sapphire",
+      "Alumina Substrate"
+    ],
+    "category": "Substrate",
+    "tags": [
+      "substrate",
+      "quantum computing",
+      "photonic",
+      "dielectric",
+      "microwave",
+      "cryogenic",
+      "low loss",
+      "single crystal",
+      "laser optics",
+      "industry:quantum",
+      "industry:photonics",
+      "industry:lasers"
+    ]
+  },
+  {
+    "name": "Zirconia Toughened Alumina (ZTA)",
+    "formula": "Al\u2082O\u2083 + ZrO\u2082",
+    "synonyms": [
+      "ZTA",
+      "Zirconia-Alumina Composite"
+    ],
+    "category": "Composite",
+    "tags": [
+      "ceramic",
+      "aerospace",
+      "refractory",
+      "thermal shock",
+      "wear resistant",
+      "structural",
+      "hard",
+      "tooling",
+      "insulator",
+      "industry:aerospace",
+      "industry:defense",
+      "industry:infrastructure"
+    ]
+  },
+  {
+    "name": "Niobium Titanium (NbTi) Wire",
+    "formula": "Nb-Ti/Cu",
+    "synonyms": [
+      "NbTi",
+      "Superconducting Wire"
+    ],
+    "category": "Superconductor",
+    "tags": [
+      "superconductor",
+      "low temperature",
+      "ductile",
+      "quantum computing",
+      "composite",
+      "wire",
+      "magnet",
+      "cryogenic",
+      "vacuum compatible",
+      "fusion",
+      "industry:quantum",
+      "industry:cryogenics",
+      "industry:energy"
+    ]
+  },
+  {
+    "name": "ZBLAN Fluoride Glass",
+    "formula": "ZrF\u2084-BaF\u2082-LaF\u2083-AlF\u2083-NaF",
+    "synonyms": [
+      "ZBLAN",
+      "Fluoride Glass"
+    ],
+    "category": "Optical Material",
+    "tags": [
+      "infrared",
+      "fiber",
+      "transparent",
+      "optical",
+      "photonic",
+      "cryogenic",
+      "low loss",
+      "space",
+      "laser",
+      "SWIR",
+      "IR transmission",
+      "industry:photonics",
+      "industry:lasers",
+      "industry:space"
     ]
   }
 ];
