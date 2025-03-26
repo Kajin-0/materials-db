1f39f307540c73c883ae328b8e8237562d9902fa
Kajin-0 <terencebfisher@gmail.com>
2025-03-26 17:29:31 -0400

    Add files via upload

--- materials.js
+++ materials.js
@@ -1,59 +1,205 @@
-
 window.materials = [
   {
     "name": "Gallium Arsenide",
     "formula": "GaAs",
-    "synonyms": ["GaAs", "Gallium Arsenide"],
-    "tags": ["III-V", "infrared", "photosensor"],
-    "category": "Semiconductor"
+    "synonyms": [
+      "GaAs",
+      "Gallium Arsenide"
+    ],
+    "category": "Semiconductor",
+    "tags": [
+      "III-V",
+      "group 13",
+      "group 15",
+      "photodiode",
+      "photosensor",
+      "photosensing",
+      "direct bandgap",
+      "semiconductor",
+      "high mobility",
+      "infrared",
+      "SWIR",
+      "epitaxial",
+      "MBE",
+      "MOCVD",
+      "single crystal",
+      "CMOS compatible",
+      "laser diode",
+      "sensor",
+      "commercial",
+      "aerospace",
+      "cryogenic",
+      "vacuum compatible"
+    ]
   },
   {
     "name": "Silicon",
     "formula": "Si",
-    "synonyms": ["Si", "Silicon"],
-    "tags": ["CMOS", "computing", "aerospace"],
-    "category": "Semiconductor"
+    "synonyms": [
+      "Si",
+      "Silicon"
+    ],
+    "category": "Semiconductor",
+    "tags": [
+      "Group IV",
+      "elemental",
+      "indirect bandgap",
+      "semiconductor",
+      "CMOS compatible",
+      "transistor",
+      "substrate",
+      "high temperature",
+      "durable",
+      "low cost",
+      "aerospace",
+      "computing",
+      "research",
+      "bulk grown"
+    ]
   },
   {
     "name": "Mercury Cadmium Telluride",
     "formula": "Hg1-xCdxTe",
-    "synonyms": ["HgCdTe"],
-    "tags": ["IR", "photosensor", "aerospace"],
-    "category": "Semiconductor"
+    "synonyms": [
+      "HgCdTe",
+      "MCT"
+    ],
+    "category": "Semiconductor",
+    "tags": [
+      "II-VI",
+      "group 12",
+      "group 16",
+      "chalcogenide",
+      "IR",
+      "MWIR",
+      "LWIR",
+      "photosensor",
+      "photodiode",
+      "photoconductor",
+      "photoconductive",
+      "tunable bandgap",
+      "semiconductor",
+      "MBE",
+      "LPE",
+      "epitaxial",
+      "single crystal",
+      "mercury",
+      "cadmium",
+      "tellurium",
+      "toxic",
+      "cryogenic",
+      "nitrogen cooled",
+      "aerospace",
+      "sensor"
+    ]
   },
   {
     "name": "Aluminum Oxide",
     "formula": "Al2O3",
-    "synonyms": ["Sapphire", "Alumina"],
-    "tags": ["insulator", "hard", "aerospace"],
-    "category": "Ceramic"
-  },
-  {
-    "name": "Carbon Fiber Reinforced Polymer",
-    "formula": "CFRP",
-    "synonyms": ["Carbon Fiber Composite"],
-    "tags": ["lightweight", "strong", "aerospace"],
-    "category": "Composite"
+    "synonyms": [
+      "Sapphire",
+      "Alumina"
+    ],
+    "category": "Ceramic",
+    "tags": [
+      "oxide",
+      "insulator",
+      "hard",
+      "corrosion resistant",
+      "thermal insulator",
+      "transparent",
+      "substrate",
+      "aerospace",
+      "vacuum compatible",
+      "durable",
+      "ceramic"
+    ]
   },
   {
     "name": "Indium Gallium Arsenide",
     "formula": "InGaAs",
-    "synonyms": ["InxGa1-xAs"],
-    "tags": ["SWIR", "infrared", "photosensor"],
-    "category": "Semiconductor"
+    "synonyms": [
+      "InxGa1-xAs",
+      "InGaAs"
+    ],
+    "category": "Semiconductor",
+    "tags": [
+      "III-V",
+      "group 13",
+      "group 15",
+      "infrared",
+      "SWIR",
+      "photosensor",
+      "photodiode",
+      "semiconductor",
+      "MBE",
+      "MOCVD",
+      "epitaxial",
+      "tunable bandgap",
+      "high mobility",
+      "sensor"
+    ]
   },
   {
     "name": "Titanium Alloy",
     "formula": "Ti-6Al-4V",
-    "synonyms": ["Ti64", "Grade 5 Titanium"],
-    "tags": ["strong", "lightweight", "aerospace"],
-    "category": "Metal"
+    "synonyms": [
+      "Ti64",
+      "Grade 5 Titanium"
+    ],
+    "category": "Metal",
+    "tags": [
+      "strong",
+      "ductile",
+      "corrosion resistant",
+      "aerospace",
+      "metal",
+      "vacuum compatible",
+      "high temperature",
+      "machinable",
+      "biocompatible",
+      "alloy"
+    ]
+  },
+  {
+    "name": "Carbon Fiber Reinforced Polymer",
+    "formula": "CFRP",
+    "synonyms": [
+      "Carbon Fiber Composite"
+    ],
+    "category": "Composite",
+    "tags": [
+      "composite",
+      "lightweight",
+      "strong",
+      "aerospace",
+      "structure",
+      "durable",
+      "high strength",
+      "low density",
+      "thermal stable",
+      "non-metallic"
+    ]
   },
   {
     "name": "Bismuth Telluride",
     "formula": "Bi2Te3",
-    "synonyms": ["BiTe", "Thermoelectric"],
-    "tags": ["thermoelectric", "cooling", "generator"],
-    "category": "Semiconductor"
+    "synonyms": [
+      "BiTe",
+      "Thermoelectric"
+    ],
+    "category": "Semiconductor",
+    "tags": [
+      "thermoelectric",
+      "semiconductor",
+      "cooling",
+      "generator",
+      "chalcogenide",
+      "group 15",
+      "group 16",
+      "vacuum compatible",
+      "cryogenic",
+      "aerospace"
+    ]
   }
 ];
