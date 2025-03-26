5e1e7c6dc67b48cd807fcebe26770f3251461c05
Kajin-0 <terencebfisher@gmail.com>
2025-03-26 16:58:17 -0400

    Add files via upload

---
+++ materials.js
@@ -0,0 +1,24 @@
+
+const materials = [
+  {
+    "name": "Gallium Arsenide",
+    "formula": "GaAs",
+    "synonyms": ["Gallium Arsenide", "GaAs"],
+    "category": "Semiconductor",
+    "tags": ["photosensor", "III-V", "infrared"]
+  },
+  {
+    "name": "Silicon",
+    "formula": "Si",
+    "synonyms": ["Si", "Silicon"],
+    "category": "Semiconductor",
+    "tags": ["CMOS-compatible", "aerospace", "photonic"]
+  },
+  {
+    "name": "HgCdTe",
+    "formula": "Hg1-xCdxTe",
+    "synonyms": ["Mercury Cadmium Telluride", "HgCdTe"],
+    "category": "Semiconductor",
+    "tags": ["IR", "photosensor", "SWIR", "aerospace"]
+  }
+];
