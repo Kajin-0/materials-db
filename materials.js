04f26d2e65ffaeb3b4f364aa807b4e5760e1ec52
Kajin-0 <terencebfisher@gmail.com>
2025-03-26 17:55:00 -0400

    Add files via upload

--- materials.js
+++ materials.js
@@ -1,5 +1,136 @@
 window.materials = [
   {
+    "name": "Gallium Arsenide",
+    "formula": "GaAs",
+    "synonyms": [
+      "GaAs",
+      "Gallium Arsenide"
+    ],
+    "category": "Semiconductor",
+    "tags": [
+      "III-V",
+      "group 13",
+      "group 15",
+      "photodiode",
+      "photosensor",
+      "photosensing",
+      "direct bandgap",
+      "semiconductor",
+      "high mobility",
+      "infrared",
+      "SWIR",
+      "epitaxial",
+      "MBE",
+      "MOCVD",
+      "single crystal",
+      "CMOS compatible",
+      "laser diode",
+      "sensor",
+      "commercial",
+      "aerospace",
+      "cryogenic",
+      "vacuum compatible",
+      "industry:aerospace",
+      "industry:photonics",
+      "industry:lasers"
+    ]
+  },
+  {
+    "name": "Silicon",
+    "formula": "Si",
+    "synonyms": [
+      "Si",
+      "Silicon"
+    ],
+    "category": "Semiconductor",
+    "tags": [
+      "Group IV",
+      "elemental",
+      "indirect bandgap",
+      "semiconductor",
+      "CMOS compatible",
+      "transistor",
+      "substrate",
+      "high temperature",
+      "durable",
+      "low cost",
+      "aerospace",
+      "computing",
+      "research",
+      "bulk grown",
+      "industry:electronics",
+      "industry:computing",
+      "industry:aerospace"
+    ]
+  },
+  {
+    "name": "Mercury Cadmium Telluride",
+    "formula": "Hg1-xCdxTe",
+    "synonyms": [
+      "HgCdTe",
+      "MCT"
+    ],
+    "category": "Semiconductor",
+    "tags": [
+      "II-VI",
+      "group 12",
+      "group 16",
+      "chalcogenide",
+      "IR",
+      "MWIR",
+      "LWIR",
+      "photosensor",
+      "photodiode",
+      "photoconductor",
+      "photoconductive",
+      "tunable bandgap",
+      "semiconductor",
+      "MBE",
+      "LPE",
+      "epitaxial",
+      "single crystal",
+      "mercury",
+      "cadmium",
+      "tellurium",
+      "toxic",
+      "cryogenic",
+      "nitrogen cooled",
+      "aerospace",
+      "sensor",
+      "industry:infrared",
+      "industry:aerospace",
+      "industry:photonics"
+    ]
+  },
+  {
+    "name": "Indium Gallium Arsenide",
+    "formula": "InGaAs",
+    "synonyms": [
+      "InxGa1-xAs",
+      "InGaAs"
+    ],
+    "category": "Semiconductor",
+    "tags": [
+      "III-V",
+      "group 13",
+      "group 15",
+      "infrared",
+      "SWIR",
+      "photosensor",
+      "photodiode",
+      "semiconductor",
+      "MBE",
+      "MOCVD",
+      "epitaxial",
+      "tunable bandgap",
+      "high mobility",
+      "sensor",
+      "industry:infrared",
+      "industry:photonics",
+      "industry:lasers"
+    ]
+  },
+  {
     "name": "Reinforced Carbon\u2013Carbon (RCC)",
     "formula": "C\u2013C + SiC",
     "synonyms": [
