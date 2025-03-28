bba640ec34bf0923ac176e18dc201825f26d54b7
Kajin-0 <terencebfisher@gmail.com>
2025-03-27 20:27:10 -0400

    Add files via upload

--- data/tagTaxonomy.js
+++ data/tagTaxonomy.js
@@ -1,74 +1,92 @@
 window.tagTaxonomy = {
-  "domains": [
-    "infrared",
-    "microwave",
-    "quantum",
-    "laser",
-    "photonics",
-    "energy",
-    "cryogenics",
-    "defense",
-    "aerospace",
-    "electronics",
-    "medical"
-  ],
-  "materialClasses": [
-    "III-V",
-    "II-VI",
-    "IV",
-    "oxide",
-    "chalcogenide",
-    "ceramic",
-    "polymer",
-    "metal",
-    "composite",
-    "superconductor",
-    "semiconductor"
-  ],
-  "properties": [
-    "cryogenic",
-    "superconductor",
-    "tunable bandgap",
-    "magnetic",
-    "non-magnetic",
-    "biocompatible",
-    "hard",
-    "ductile",
-    "flexible",
-    "transparent",
-    "abrasion resistant",
-    "radiation hardened",
-    "corrosion resistant",
-    "electrically conductive",
-    "thermal conductive",
-    "electrically insulating",
-    "low loss",
-    "wear resistant",
-    "sensor",
-    "photosensitive"
-  ],
-  "processes": [
-    "MBE",
-    "MOCVD",
-    "LPE",
-    "coated conductor",
-    "epitaxial",
-    "bulk grown",
-    "CVD",
-    "sputtered",
-    "doped",
-    "annealed",
-    "fiber drawn"
-  ],
-  "environments": [
-    "vacuum compatible",
-    "high temperature",
-    "low temperature",
-    "thermal protection",
-    "space rated",
-    "pressure resistant",
-    "oxidation resistant",
-    "non-stick",
-    "UV resistant"
-  ]
-};
+  "class": {
+    "label": "Material Class",
+    "tags": [
+      "semiconductor",
+      "II-VI",
+      "III-V",
+      "chalcogenide",
+      "group 12",
+      "group 13",
+      "group 15",
+      "group 16"
+    ]
+  },
+  "prop": {
+    "label": "Properties",
+    "tags": [
+      "cryogenic",
+      "toxic",
+      "tunable_bandgap",
+      "high_mobility",
+      "low_loss",
+      "hard",
+      "lightweight",
+      "corrosion_resistant",
+      "thermal_insulation",
+      "wear_resistant"
+    ]
+  },
+  "proc": {
+    "label": "Processes",
+    "tags": [
+      "MBE",
+      "MOCVD",
+      "LPE",
+      "epitaxial",
+      "single_crystal"
+    ]
+  },
+  "app": {
+    "label": "Applications",
+    "tags": [
+      "photodiode",
+      "photosensor",
+      "photoconductor",
+      "sensor",
+      "laser"
+    ]
+  },
+  "industry": {
+    "label": "Industry",
+    "tags": [
+      "aerospace",
+      "photonics",
+      "infrared",
+      "lasers",
+      "space",
+      "defense",
+      "electronics",
+      "energy",
+      "quantum",
+      "cryogenics"
+    ]
+  },
+  "env": {
+    "label": "Environment",
+    "tags": [
+      "vacuum_compatible",
+      "nitrogen_cooled"
+    ]
+  },
+  "domain": {
+    "label": "Domain",
+    "tags": [
+      "infrared",
+      "SWIR",
+      "MWIR",
+      "LWIR"
+    ]
+  },
+  "element": {
+    "label": "Constituent Elements",
+    "tags": [
+      "mercury",
+      "cadmium",
+      "tellurium",
+      "indium",
+      "gallium",
+      "arsenic"
+    ]
+  }
+}
